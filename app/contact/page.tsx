a9410795321a50d4ecb7633f8799b4eb5fd160a1
HiraMubashir <hiraamjad425@gmail.com>
2025-03-03 20:25:31 +0500

    updated privacy policy

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,30 @@
 import React from "react";
-import styles from "../../styles/ContactUs.module.css"; // Import the CSS Module
+import Head from "next/head"; // Import Head component
+import styles from "../../styles/ContactUs.module.css";
 
 const ContactUs = () => {
   return (
-    <div className={styles.container}>
-      <h1>Talk to Us</h1>
-      <form className={styles.form}>
-        <label className={styles.label}>Name</label>
-        <input type="text" className={styles.input} placeholder="Your Name" />
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
+      
+      <div className={styles.container}>
+        <h1>Talk to Us</h1>
+        <form className={styles.form}>
+          <label className={styles.label}>Name</label>
+          <input type="text" className={styles.input} placeholder="Your Name" />
 
-        <label className={styles.label}>E-mail</label>
-        <input type="email" className={styles.input} placeholder="Your Email" />
+          <label className={styles.label}>E-mail</label>
+          <input type="email" className={styles.input} placeholder="Your Email" />
 
-        <label className={styles.label}>Message</label>
-        <textarea className={styles.textarea} placeholder="Your Message"></textarea>
+          <label className={styles.label}>Message</label>
+          <textarea className={styles.textarea} placeholder="Your Message"></textarea>
 
-        <button type="submit" className={styles.button}>Send Message</button>
-      </form>
-    </div>
+          <button type="submit" className={styles.button}>Send Message</button>
+        </form>
+      </div>
+    </>
   );
 };
 
